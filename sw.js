10afcef7fc3acb99c12aa76f12fb88eab367f3e8
CWatt250 <wattattack@yahoo.com>
2026-04-28 19:13:50 -0700

    Travel time persists across cache hits, SW v7

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // SubWatt service worker
 // Bump CACHE_NAME when shipping new assets so clients pick them up
-const CACHE_NAME = 'subwatt-v6';
+const CACHE_NAME = 'subwatt-v7';
 const TILE_CACHE = 'subwatt-tiles-v1';
 const MAX_TILES = 500;
 
